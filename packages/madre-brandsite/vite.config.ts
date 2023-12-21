31cb83278705651dcc8e7028cfc2516bba765076
rlawnsxo131 <rlawnsxo131@gmail.com>
2023-12-22 03:58:20 +0900

    style(shared, brandsite): scss 변수 이동

--- packages/madre-brandsite/vite.config.ts
+++ packages/madre-brandsite/vite.config.ts
@@ -1,7 +1,13 @@
 import { unstable_vitePlugin as remix } from '@remix-run/dev';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [remix(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'app'),
+    },
+  },
 });
