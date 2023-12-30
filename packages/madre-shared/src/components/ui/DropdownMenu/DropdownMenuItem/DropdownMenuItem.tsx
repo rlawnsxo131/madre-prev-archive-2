65737428083c1f508481e0efa277dcb19eb84080
rlawnsxo131 <rlawnsxo131@gmail.com>
2023-12-31 05:59:32 +0900

    feat(shared): DropdownMenu 작성중

--- packages/madre-shared/src/components/ui/DropdownMenu/DropdownMenuItem/DropdownMenuItem.tsx
+++ packages/madre-shared/src/components/ui/DropdownMenu/DropdownMenuItem/DropdownMenuItem.tsx
@@ -1,30 +1,38 @@
 import classNames from 'classnames';
-import { type HTMLAttributes } from 'react';
+import { cloneElement, type HTMLAttributes } from 'react';
 
 import { useVisibleContext } from '../../../../contexts/VisibleContext';
 import { Slot } from '../../../utility/Slot';
 import styles from './DropdownMenuItem.module.scss';
+
+type DropdownMenuItemProps = {
+  children: JSX.Element;
+  className?: string;
+  onClick?: () => void | Promise<void>;
+};
 
 export function DropdownMenuItem({
   children,
   className,
   onClick,
   ...props
-}: HTMLAttributes<HTMLLIElement> & {
-  children: JSX.Element;
-}) {
+}: DropdownMenuItemProps) {
   const { close } = useVisibleContext();
 
   return (
-    <li
-      className={classNames(styles.DropdownMenuItem, className)}
-      onClick={(e) => {
-        close();
-        onClick?.(e);
-      }}
-      {...props}
-    >
-      <Slot {...{ ['data-john']: '' }}>{children}</Slot>
+    <li className={classNames(styles.DropdownMenuItem, className)} {...props}>
+      {cloneElement(children, {
+        ['data-john']: '',
+        onClick: () =>
+          new Promise((resolve) => {
+            if (onClick) {
+              resolve(onClick());
+            }
+            resolve(true);
+          })
+            .then(() => children.props.onClick?.())
+            .then(() => close()),
+      })}
     </li>
   );
 }
--- packages/madre-shared/src/components/ui/DropdownMenu/DropdownMenuTrigger/DropDownMenuTrigger.tsx
+++ packages/madre-shared/src/components/ui/DropdownMenu/DropdownMenuTrigger/DropDownMenuTrigger.tsx
@@ -1,27 +1,35 @@
 import classNames from 'classnames';
-import { type HTMLAttributes } from 'react';
+import { cloneElement } from 'react';
 
 import { useVisibleContext } from '../../../../contexts/VisibleContext';
 import styles from './DropdownMenuTrigger.module.scss';
+
+type DropdownMenuTriggerProps = {
+  children: JSX.Element;
+  className?: string;
+  onClick?: () => void | Promise<void>;
+};
 
 export function DropdownMenuTrigger({
   children,
   className,
   onClick,
-  ...props
-}: HTMLAttributes<HTMLDivElement>) {
+}: DropdownMenuTriggerProps) {
   const { toggle } = useVisibleContext();
 
   return (
-    <div
-      className={classNames(styles.DropdownMenuTrigger, className)}
-      onClick={(e) => {
-        toggle();
-        onClick?.(e);
-      }}
-      {...props}
-    >
-      {children}
+    <div className={classNames(styles.DropdownMenuTrigger, className)}>
+      {cloneElement(children, {
+        onClick: () =>
+          new Promise((resolve) => {
+            if (onClick) {
+              resolve(onClick());
+            }
+            resolve(true);
+          })
+            .then(() => children.props.onClick?.())
+            .then(() => toggle()),
+      })}
     </div>
   );
 }
