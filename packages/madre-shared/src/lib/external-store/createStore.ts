f6619cd313f775994749885e00776f39a1cf0f3f
rlawnsxo131 <rlawnsxo131@gmail.com>
2024-01-05 10:11:31 +0900

    chore(feat): 계속 타입 삽질중)

--- packages/madre-shared/src/lib/external-store/createStore.ts
+++ packages/madre-shared/src/lib/external-store/createStore.ts
@@ -16,7 +16,7 @@ export function createStore<State extends TState<State>>(
   const api = createExternalStoreApi(createState);
 
   return () => useExternalStore(api, api.getState);
-  // return (selector) => useExternalStore(api, selector);
+  // return (selector) => useExternalStore(api, selector); // 이거하고싶어
 }
 
 export const store = create<{ name: string; name2: string }>((set) => ({
@@ -34,8 +34,8 @@ const useStore = createStore<{ name: string; name2: string }>((set) => ({
 }));
 
 export function A() {
-  const _1 = useStore((state) => ({ name: state.name }));
-  // const _2 = useStore((state) => state.name);
-  // const _3 = useStore((state) => ({ name2: state.name2 }));
-  // const _4 = useStore((state) => ({ name: state.name, name2: state.name2 }));
+  const _1 = useStore();
+  // const _2 = useStore((state) => state.name); // 이거하고 싶다
+  // const _3 = useStore((state) => ({ name2: state.name2 })); // 이거하고싶어
+  // const _4 = useStore((state) => ({ name: state.name, name2: state.name2 })); // 아아 이거
 }
