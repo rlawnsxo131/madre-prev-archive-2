4107a64672f24cc7ff260b53851dfe32343a4443
rlawnsxo131 <rlawnsxo131@gmail.com>
2024-01-08 21:06:36 +0900

    refactor(shared): eventEmitter event 객체 숨김처리

--- packages/madre-shared/src/lib/event/createEventEmitter.ts
+++ packages/madre-shared/src/lib/event/createEventEmitter.ts
@@ -2,23 +2,25 @@ export type EventsMap = { [event: string]: any };
 export type DefaultEvents = {
   [event: string]: (...args: any[]) => void;
 };
+export type PartialEvents<Events> = Partial<{
+  [E in keyof Events]: Set<Events[E]>;
+}>;
 
-export interface EventEmitter<Events extends EventsMap = DefaultEvents> {
-  events: Partial<{ [E in keyof Events]: Set<Events[E]> }>;
-  on<K extends keyof Events>(this: this, event: K, cb: Events[K]): () => void;
+export type EventEmitter<Events extends EventsMap = DefaultEvents> = {
+  on<K extends keyof Events>(event: K, cb: Events[K]): () => void;
   emit: <K extends keyof Events>(
-    this: this,
     event: K,
     ...args: Parameters<Events[K]>
   ) => void;
+  get: () => PartialEvents<Events>;
   clear: () => void;
-}
+};
 
 /**
  * @description 간단한 event emitter 입니다.
  *
  * @example
- * interface Events {
+ * type Events = {
  *   set: (name: string, count: number) => void;
  *   tick: (volumn: number) => void;
  * }
@@ -37,21 +39,23 @@ export interface EventEmitter<Events extends EventsMap = DefaultEvents> {
 export function createEventEmitter<
   Events extends EventsMap = DefaultEvents,
 >(): EventEmitter<Events> {
+  let events: PartialEvents<Events> = {};
+
   return {
-    events: {},
-    on(event, cb) {
-      (this.events[event] ??= new Set()).add(cb);
+    on: (event, cb) => {
+      (events[event] ??= new Set()).add(cb);
       return () => {
-        this.events[event]?.delete(cb);
+        events[event]?.delete(cb);
       };
     },
-    emit(event, ...args) {
-      for (const callback of this.events[event] || []) {
+    emit: (event, ...args) => {
+      for (const callback of events[event] || []) {
         callback(...args);
       }
     },
+    get: () => events,
     clear() {
-      this.events = {};
+      events = {};
     },
   };
 }
