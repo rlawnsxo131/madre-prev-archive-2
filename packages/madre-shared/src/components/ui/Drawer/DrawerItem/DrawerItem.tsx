3163d35c381b4d3b0060d96194acae1c12fa9267
rlawnsxo131 <rlawnsxo131@gmail.com>
2024-01-10 06:03:51 +0900

    chore(shared): something works

--- packages/madre-shared/src/components/ui/Drawer/DrawerItem/DrawerItem.tsx
+++ packages/madre-shared/src/components/ui/Drawer/DrawerItem/DrawerItem.tsx
@@ -1,15 +1,27 @@
 import classNames from 'classnames';
-import { forwardRef, type PropsWithoutRef } from 'react';
+import {
+  Children,
+  cloneElement,
+  forwardRef,
+  type PropsWithoutRef,
+} from 'react';
 
+import { useDrawerActions } from '../DrawerProvider';
 import styles from './DrawerItem.module.scss';
 
 export const DrawerItem = forwardRef<
   HTMLDivElement,
   PropsWithoutRef<{ children: JSX.Element; className?: string }>
 >(({ children, className }, ref) => {
+  const child = Children.only(children);
+  const { hide } = useDrawerActions();
+
   return (
     <div ref={ref} className={classNames(styles.DrawerItem, className)}>
-      {children}
+      {cloneElement(children, {
+        onClick: () =>
+          Promise.resolve(child.props.onClick?.()).then(() => hide()),
+      })}
     </div>
   );
 });
