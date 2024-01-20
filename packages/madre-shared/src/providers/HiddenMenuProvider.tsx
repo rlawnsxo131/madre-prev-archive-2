f68392d8f4e3a510fd85962b2ea1848973a4fdfb
rlawnsxo131 <rlawnsxo131@gmail.com>
2024-01-21 05:52:49 +0900

    comment: 주석 추가

--- packages/madre-shared/src/providers/HiddenMenuProvider.tsx
+++ packages/madre-shared/src/providers/HiddenMenuProvider.tsx
@@ -9,11 +9,9 @@ import {
 
 import { useSafeContext } from '../hooks/useSafeContext';
 
-// state
 const HiddenMenuStateContext = createContext<{ visible: boolean } | null>(null);
 HiddenMenuStateContext.displayName = 'HiddenMenuStateContext';
 
-// actions
 const HiddenMenuActionsContext = createContext<{
   set: (visible: boolean) => void;
   open: () => void;
@@ -32,6 +30,36 @@ export type HiddneMenuProviderProps<
   };
 };
 
+/**
+ * @description 특정 액션을 통해 노출여부를 결정하는 컴포넌트에서 공통으로 사용중인 Provider 입니다.
+ * lifeCycle 을 통해 state 를 변경전 action 을 실행할 수 있도록 합니다.
+ * before action 만 지원하는 이유는, 해당 provider 를 사용하여 노출 유무를 선택하는 컴포넌트가
+ * after event 를 컨트롤할 수 있도록 인터페이스를 열어두기 때문 입니다.
+ *
+ * @description 예를 들어 trigger 에 onClick event 를 넘긴다면 close/open 이벤트와 동시에 onClick 이벤트를 실행합니다.
+ * close/open 이벤트 실행바로 뒤 onClick 이벤트가 작동하는데, 이때 visible state 의 변화는 즉각 트리거 되므로,
+ * 해당 이벤트가 실행되었음을 보장합니다. UX 상 닫히는 transition 이 작동함과 동시에 해당 이벤트가 실행 되는것은 큰 지장이 없으므로
+ * 위와같이 이벤트를 핸들링 합니다.
+ * 
+ * @example
+ * const child = Children.only(children);
+ * const { close } = useDropdownMenuActions();
+ * 
+ * return (
+ *  <li ref={ref} className={classNames(styles.DropdownMenuItem, className)}>
+ *    {cloneElement(children, {
+ *      onClick: () => {
+ *        close();
+ *        child.props.onClick?.();
+ *      },
+ *    })}
+ *  </li>
+  );
+ *
+ * @param children
+ * @param lifeCycle
+ *
+ */
 export function HiddenMenuProvider({
   children,
   lifeCycle,
