9930d8a57c605ce49eac1ecb0a7221c693a78d73
rlawnsxo131 <rlawnsxo131@gmail.com>
2024-01-21 05:43:28 +0900

    feat: Accordion 작성중

--- packages/madre-shared/src/components/ui/Accordion/Accordion.tsx
+++ packages/madre-shared/src/components/ui/Accordion/Accordion.tsx
@@ -1,3 +1,12 @@
+import { AccordionContent } from './AccordionContent';
+import { AccordionItem } from './AccordionItem';
 import { AccordionProvider } from './AccordionProvider';
+import { AccordionRoot } from './AccordionRoot';
+import { AccordionTrigger } from './AccordionTrigger';
 
-export const Accordion = Object.assign(AccordionProvider, {});
+export const Accordion = Object.assign(AccordionProvider, {
+  Root: AccordionRoot,
+  Trigger: AccordionTrigger,
+  Content: AccordionContent,
+  Item: AccordionItem,
+});
--- packages/madre-shared/src/components/ui/Accordion/AccordionContent/AccordionContent.tsx
+++ packages/madre-shared/src/components/ui/Accordion/AccordionContent/AccordionContent.tsx
@@ -1,12 +1,27 @@
-import { forwardRef, type PropsWithoutRef } from 'react';
+import classNames from 'classnames';
+import { AnimatePresence, motion } from 'framer-motion';
+import { forwardRef, type PropsWithoutRef, type ReactNode } from 'react';
 
+import { useAccordionState } from '../AccordionProvider';
 import styles from './AccordionContent.module.scss';
 
 export type AccordionContentProps = PropsWithoutRef<{}>;
 
 export const AccordionContent = forwardRef<
-  HTMLDivElement,
-  AccordionContentProps
->((props, ref) => {
-  return <div ref={ref} className={styles.AccordionContent}></div>;
+  HTMLUListElement,
+  PropsWithoutRef<{ children: ReactNode; className?: string }>
+>(({ children, className }, ref) => {
+  const { visible } = useAccordionState();
+
+  return (
+    <AnimatePresence>
+      {visible && (
+        <div className={styles.AccordionContent}>
+          <motion.ul ref={ref} className={classNames(styles.items, className)}>
+            {children}
+          </motion.ul>
+        </div>
+      )}
+    </AnimatePresence>
+  );
 });
