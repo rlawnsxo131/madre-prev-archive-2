339d65984b09f2f1586bd2bbeafb2d20ec731846
rlawnsxo131 <rlawnsxo131@gmail.com>
2024-02-20 01:52:17 +0900

    fix(infra): fix mysql index

--- infra/rdb/init.sql
+++ infra/rdb/init.sql
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS madre.account_social_provider(
   `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `ix_user_id` (`account_id`),
+  UNIQUE KEY `ix_account_id` (`account_id`),
   UNIQUE KEY `ix_social_id_provider` (`social_id`, `provider`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
